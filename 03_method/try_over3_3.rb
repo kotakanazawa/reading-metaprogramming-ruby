ae61871ce34978da0011bb85da58f4a027037945
otegami <a.s.takuya1026@gmail.com>
2021-12-22 08:41:06 +0900

    Implemented try_over3 Q5

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -110,17 +110,37 @@ end
 # TryOver3::TaskHelper という include すると task というクラスマクロが与えられる以下のようなモジュールがあります。
 module TryOver3::TaskHelper
   def self.included(klass)
-    klass.define_singleton_method :task do |name, &task_block|
-      new_klass = Class.new do
-        define_singleton_method :run do
+    klass.class_eval do
+      @klass_names = {}
+
+      define_singleton_method :task do |name, &task_block|
+        new_klass_name = name.to_s.split("_").map{ |w| w[0] = w[0].upcase; w }.join
+        @klass_names[new_klass_name] = task_block
+  
+        define_singleton_method "#{name}" do
           puts "start #{Time.now}"
           block_return = task_block.call
           puts "finish #{Time.now}"
           block_return
         end
       end
-      new_klass_name = name.to_s.split("_").map{ |w| w[0] = w[0].upcase; w }.join
-      const_set(new_klass_name, new_klass)
+  
+      define_singleton_method :const_missing do |const_name|
+        super(const_name) unless @klass_names.include?(const_name.to_s)
+
+        klass_name = self.name
+        task_block = @klass_names[const_name.to_s]
+
+        Class.new do
+          define_singleton_method :run do
+            warn "Warning: #{klass_name}::#{const_name.to_s}.run is deprecated"
+            puts "start #{Time.now}"
+            block_return = task_block.call
+            puts "finish #{Time.now}"
+            block_return
+          end
+        end
+      end
     end
   end
 end
